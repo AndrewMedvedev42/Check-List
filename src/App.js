63f5384dc4710c8f9b16ec7d28f7c04a97e6c8b6
andrewNEW42 <sanjino56@gmail.com>
2021-08-03 13:47:18 +0300

    added list stucture

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import {createElement, React, useState} from "react"
+import {CheckListObject} from "./CheckListObject"
 
 function App() {
+
+  // const [checked, setChecked] = useState(false)
+ 
+  // const handleChange = () => {
+  //   setChecked(!checked);
+  // };
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <section className="App">
+      <div>
+        <h1>My startup progress</h1>
+        {CheckListObject.map((item)=>{
+          const {phaseName, tasks} = item
+          return (
+            <section>
+              <h1>{phaseName}</h1>
+              <label>
+                {tasks.map((item)=>{
+                  return (<div>
+                            <input type="checkbox"/><span>{item.taskName}</span>
+                          </div> )
+                })}
+              </label>
+            </section>
+          )
+        })}
+        {/* <label>
+        <input type="checkbox" checked={checked} onChange={handleChange}/>My Value
+        </label> */}
+      </div>
+    </section>
   );
 }
 
---
+++ src/CheckListObject.js
@@ -0,0 +1,5 @@
+export const CheckListObject = [
+    {phaseName:"Phase №1", tasks:[{taskName:"1 task1"},{taskName:"1 task2"}]},
+    {phaseName:"Phase №2", tasks:[{taskName:"2 task1"}]},
+    {phaseName:"Phase №3", tasks:[{taskName:"3 task1"},{taskName:"3 task2"}]},
+]
