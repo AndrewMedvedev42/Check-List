949c185fc29fad49b7f6b4c5d1ddc49755e42afe
andrewNEW42 <sanjino56@gmail.com>
2021-08-06 20:48:09 +0300

    again able to change check status of checkbox in localstorage

--- src/CheckListObject.js
+++ src/CheckListObject.js
@@ -1,20 +1,20 @@
 export const PhaseOneList = [
     {phaseName:"Phase №1", 
         tasks:[
-            {id:"Phase1Task1",taskName:"task", isChecked:false},
-            {id:"Phase1Task2", taskName:"task", isChecked:false}]
+            {id:"Ph-1,T-1",taskName:"task", isChecked:false},
+            {id:"Ph-1,T-2", taskName:"task", isChecked:false}]
         }]
 
 export const PhaseTwoList = [
     {phaseName:"Phase №2", 
         tasks:[
-            {id:"Phase2Task1",taskName:"task", isChecked:false},
-            {id:"Phase2Task2", taskName:"task", isChecked:false}]
+            {id:"Ph-2,T-1",taskName:"task", isChecked:false},
+            {id:"Ph-2,T-2", taskName:"task", isChecked:false}]
         }]
 
 export const PhaseThreeList = [
     {phaseName:"Phase №3", 
         tasks:[
-            {id:"Phase3Task1",taskName:"task", isChecked:false},
-            {id:"Phase3Task2", taskName:"task", isChecked:false}]
+            {id:"Ph-3,T-1",taskName:"task", isChecked:false},
+            {id:"Ph-3,T-2", taskName:"task", isChecked:false}]
         }]
--- src/components/ListTemple.jsx
+++ src/components/ListTemple.jsx
@@ -1,5 +1,15 @@
 export const ListTemple = ({data}) => {
     const [list] = data
+
+    const handleChange = (e) => {
+        let CheckBoxStatus = JSON.parse(localStorage.getItem(e))
+        if(CheckBoxStatus.checkStatus){
+          localStorage.setItem(e, JSON.stringify({checkStatus:false}))
+        }else{
+          localStorage.setItem(e, JSON.stringify({checkStatus:true}))
+        }
+    }
+
     return (
         <div>
             <h1>{list.phaseName}</h1>
@@ -7,9 +17,14 @@ export const ListTemple = ({data}) => {
                 {
                     list.tasks.map((item)=>{
                         const {id, taskName} = item
+                        if (localStorage.length === 6) {
+                            console.log("storage is full");
+                          }else{
+                            localStorage.setItem(id , JSON.stringify({checkStatus:false}));
+                        }
                         return (
                                 <div>
-                                    <input key={id} type="checkbox"/><span>{taskName}</span>
+                                    <input key={id} type="checkbox" onChange={()=>{handleChange(item.id)}}/><span>{taskName}</span>
                                 </div>)
                     })
                 }
