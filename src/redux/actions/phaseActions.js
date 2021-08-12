7831f96c44a32052041977d3cdb76ab5a0d92b18
andrewNEW42 <sanjino56@gmail.com>
2021-08-12 13:48:51 +0300

    now instead of array of true or false, it will return concluded summary with one boolean statement depending on statuses of checkboxes in each phase

--- src/redux/actions/phaseActions.js
+++ src/redux/actions/phaseActions.js
@@ -1,26 +1,44 @@
+
+const concludeStatus = (e) => {
+    if (e.includes(false)) {
+      return false
+    } else {
+      return true
+    }
+  };
+
 export const loadPhase1Stats = (data) => async (dispatch) => {
+
+    const statusSummary = concludeStatus(data)
+
     dispatch({
         type:"LOAD_PHASE1_STATS",
         payload:{
-            data: data,
+            data: statusSummary,
         }
     })
 }
 
 export const loadPhase2Stats = (data) => async (dispatch) => {
+
+    const statusSummary = concludeStatus(data)
+
     dispatch({
         type:"LOAD_PHASE2_STATS",
         payload:{
-            data: data,
+            data: statusSummary,
         }
     })
 }
 
 export const loadPhase3Stats = (data) => async (dispatch) => {
+
+    const statusSummary = concludeStatus(data)
+
     dispatch({
         type:"LOAD_PHASE3_STATS",
         payload:{
-            data: data,
+            data: statusSummary,
         }
     })
 }
