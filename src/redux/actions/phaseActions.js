56a5110b287fc73b757b32b8258374a1436fadb9
andrewNEW42 <sanjino56@gmail.com>
2021-09-07 19:32:20 +0300

    added file with action constants

--- src/redux/actions/phaseActions.js
+++ src/redux/actions/phaseActions.js
@@ -3,6 +3,10 @@
 //to sum up all given statuses into one respond
 //If "concludeStatus" finds false in given array, it will return false as there not all checkboxes are selected,
 //The result will be stored i  Redux store
+
+import { LOAD_PHASE_1_STATS, LOAD_PHASE_2_STATS, LOAD_PHASE_3_STATS } from "../actions/action-constants";
+
+
 const concludeStatus = (e) => {
     if (e.includes(false)) {
       return false
@@ -14,7 +18,7 @@ const concludeStatus = (e) => {
 export const loadPhase1Stats = (data) => async (dispatch) => {
     const statusSummary = concludeStatus(data)
     dispatch({
-        type:"LOAD_PHASE1_STATS",
+        type:LOAD_PHASE_1_STATS,
         payload:{
             data: statusSummary,
         }
@@ -24,7 +28,7 @@ export const loadPhase1Stats = (data) => async (dispatch) => {
 export const loadPhase2Stats = (data) => async (dispatch) => {
     const statusSummary = concludeStatus(data)
     dispatch({
-        type:"LOAD_PHASE2_STATS",
+        type: LOAD_PHASE_2_STATS,
         payload:{
             data: statusSummary,
         }
@@ -34,7 +38,7 @@ export const loadPhase2Stats = (data) => async (dispatch) => {
 export const loadPhase3Stats = (data) => async (dispatch) => {
     const statusSummary = concludeStatus(data)
     dispatch({
-        type:"LOAD_PHASE3_STATS",
+        type:LOAD_PHASE_3_STATS,
         payload:{
             data: statusSummary,
         }
--- src/redux/reducers/Phase-Reducer.js
+++ src/redux/reducers/Phase-Reducer.js
@@ -1,3 +1,5 @@
+import { LOAD_PHASE_1_STATS, LOAD_PHASE_2_STATS, LOAD_PHASE_3_STATS } from "../actions/action-constants";
+
 const initState = {
     phase1:false,
     phase2:false,
@@ -8,11 +10,11 @@ const initState = {
 
 const phaseReducer = (state=initState, action) => {
     switch (action.type){
-        case "LOAD_PHASE1_STATS":
+        case LOAD_PHASE_1_STATS:
             return {...state, phase1:action.payload.data}
-        case "LOAD_PHASE2_STATS":
+        case LOAD_PHASE_2_STATS:
             return {...state, phase2:action.payload.data}
-        case "LOAD_PHASE3_STATS":
+        case LOAD_PHASE_3_STATS:
             return {...state, phase3:action.payload.data}
         default:
             return {...state}
